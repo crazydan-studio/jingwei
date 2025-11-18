84b2f9931b2b9ade86a12c1e8bbff99e81f862a5
flytreeleft <flytreeleft@crazydan.org>
2025-11-18 17:14:09 +0800

    UI DSL: 完善基础模型定义

---
+++ 200.ui/dsl/src/main/java/io/crazydan/jingwei/ui/XuiErrors.java
@@ -0,0 +1,37 @@
+/*
+ * 精卫（JingWei） - 衔木石填沧海，筑屏障护安全
+ * Copyright (C) 2025 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.jingwei.ui;
+
+import io.nop.api.core.exceptions.ErrorCode;
+
+import static io.nop.api.core.exceptions.ErrorCode.define;
+import static io.nop.core.CoreErrors.ARG_NAME;
+import static io.nop.core.CoreErrors.ARG_VALUE;
+
+/**
+ *
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2025-11-17
+ */
+public interface XuiErrors {
+
+    ErrorCode ERR_UI_XXX = //
+            define("jingwei.err.ui.xxx", "大语言模型返回的结果不是数字:name={name},value={name}", ARG_NAME, ARG_VALUE);
+}
