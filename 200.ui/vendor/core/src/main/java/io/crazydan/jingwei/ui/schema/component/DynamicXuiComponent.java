89681df4d110c562d8c1d7a8cf1f4f6f71b7fb0c
flytreeleft <flytreeleft@crazydan.org>
2025-11-21 22:40:26 +0800

    UI Vendor: 构造 DynamicXuiComponent、DynamicXuiPage

---
+++ 200.ui/vendor/core/src/main/java/io/crazydan/jingwei/ui/schema/component/DynamicXuiComponent.java
@@ -0,0 +1,9 @@
+package io.crazydan.jingwei.ui.schema.component;
+
+import io.crazydan.jingwei.ui.schema.component._gen._DynamicXuiComponent;
+
+public class DynamicXuiComponent extends _DynamicXuiComponent{
+    public DynamicXuiComponent(){
+
+    }
+}
---
+++ 200.ui/vendor/core/src/main/java/io/crazydan/jingwei/ui/schema/page/DynamicXuiPage.java
@@ -0,0 +1,9 @@
+package io.crazydan.jingwei.ui.schema.page;
+
+import io.crazydan.jingwei.ui.schema.page._gen._DynamicXuiPage;
+
+public class DynamicXuiPage extends _DynamicXuiPage{
+    public DynamicXuiPage(){
+
+    }
+}
