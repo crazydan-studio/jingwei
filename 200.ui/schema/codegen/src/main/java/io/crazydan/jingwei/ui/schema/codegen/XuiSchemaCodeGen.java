b4e2bb6788d33e1c56cea447de88eddc9487a125
flytreeleft <flytreeleft@crazydan.org>
2025-11-24 17:47:56 +0800

    UI Schema: 支持在 IDE 中执行 codegen

---
+++ 200.ui/schema/codegen/src/main/java/io/crazydan/jingwei/ui/schema/codegen/XuiSchemaCodeGen.java
@@ -0,0 +1,60 @@
+/*
+ * 精卫（JingWei） - 衔木石填沧海，筑屏障护安全
+ * Copyright (C) 2025 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.jingwei.ui.schema.codegen;
+
+import java.io.File;
+
+import io.nop.api.core.config.AppConfig;
+import io.nop.codegen.XCodeGenerator;
+import io.nop.commons.util.MavenDirHelper;
+import io.nop.core.CoreConfigs;
+import io.nop.core.CoreConstants;
+import io.nop.core.initialize.CoreInitialization;
+
+/**
+ * 便于通过 IDE 执行 {@link #main} 函数以生成 xdsl 模型代码，
+ * 或者用于调试从 *.xgen 生成代码过程中的问题
+ *
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2025-11-24
+ */
+public class XuiSchemaCodeGen {
+
+    public static void main(String[] args) {
+        AppConfig.getConfigProvider()
+                 .updateConfigValue(CoreConfigs.CFG_CORE_MAX_INITIALIZE_LEVEL,
+                                    CoreConstants.INITIALIZER_PRIORITY_ANALYZE);
+
+        CoreInitialization.initialize();
+        try {
+            run();
+        } finally {
+            CoreInitialization.destroy();
+        }
+    }
+
+    public static void run() {
+        File projectDir = MavenDirHelper.projectDir(XuiSchemaCodeGen.class);
+
+        XCodeGenerator.runPrecompile(projectDir, "/", false);
+        XCodeGenerator.runPrecompile2(projectDir, "/", false);
+        XCodeGenerator.runPostcompile(projectDir, "/", false);
+    }
+}
