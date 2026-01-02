d97f78134a1bf8735c80cec5dbd39f3c4dbcc44a
flytreeleft <flytreeleft@crazydan.org>
2026-01-02 13:36:39 +0800

    App: 初步验证从 AI 提示词到 orm 模型的生成过程

---
+++ 100.app/src/test/java/io/crazydan/jingwei/ai/prompt/TestAiPrompt.java
@@ -0,0 +1,75 @@
+/*
+ * 精卫（JingWei） - 衔木石填沧海，筑屏障护安全
+ * Copyright (C) 2026 Crazydan Studio <https://studio.crazydan.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.
+ * If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html#license-text>.
+ */
+
+package io.crazydan.jingwei.ai.prompt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.crazydan.duzhou.framework.junit.NopJunitAutoTestCase;
+import io.nop.ai.coder.orm.AiOrmModel;
+import io.nop.ai.core.api.chat.AiChatOptions;
+import io.nop.ai.core.command.AiCommand;
+import io.nop.ai.core.prompt.IPromptTemplate;
+import io.nop.ai.core.prompt.IPromptTemplateManager;
+import io.nop.api.core.annotations.autotest.EnableSnapshot;
+import io.nop.api.core.annotations.autotest.NopTestConfig;
+import io.nop.core.lang.xml.XNode;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author <a href="mailto:flytreeleft@crazydan.org">flytreeleft</a>
+ * @date 2026-01-01
+ */
+@NopTestConfig(testConfigFile = "classpath:/application.yaml", localDb = true)
+public class TestAiPrompt extends NopJunitAutoTestCase {
+    @Inject
+    IPromptTemplateManager promptTemplateManager;
+
+    @EnableSnapshot
+    @Test
+    public void test_erd_design() {
+        IPromptTemplate promptTemplate = loadPrompt("/jingwei/ai/prompts/coder/erd-design.prompt.yaml");
+
+        AiCommand command = AiCommand.create();
+        AiChatOptions options = command.makeChatOptions();
+        options.setProvider("bailian");
+        options.setModel("qwen-coder-plus");
+
+//        // 对应系统提示词 /nop/ai/prompts/system/programming
+//        options.setWorkMode("programming");
+//        options.setEnableCognitivePrompt(true);
+//        options.setEnableMetaPrompt(true);
+//        options.setEnableSystemPrompt(true);
+
+        Map<String, Object> vars = new HashMap<>();
+        vars.put("requirements", inputText("input-requirements.md"));
+
+        command.promptTemplate(promptTemplate);
+
+        XNode node = ((AiOrmModel) command.execute(vars, null).getOutput("RESULT")).getOrmNodeForAi();
+        node.dump();
+    }
+
+    protected IPromptTemplate loadPrompt(String promptPath) {
+        return this.promptTemplateManager.loadPromptTemplateFromPath(promptPath);
+    }
+}
